367feef6ef3797a7c13bcf45db8499a8c45fa248
Zakaria Mesror <zakaria.mesror@protonmail.com>
2023-04-26 01:21:58 +0200

    Fix to_int_string function

--- binary.c
+++ binary.c
@@ -12,29 +12,26 @@ int to_binary_string(va_list ap)
 {
 	unsigned int n = va_arg(ap, unsigned int);
 	char buffer[50];
-	int i, r;
+	int i = 0, r = 0;
 	char hex = (buffer[0] == 'A') ? 'A' : 'a';
 
-	i = 0;
-	(n == 0) && (buffer[i] = '0');
-	i++;
+	(n == 0) && (buffer[i++] = '0');
 
 	while (n > 0)
 	{
 		r = n % 2;
 
-	if (r < 10)
-	{
-		buffer[i] = r + '0';
+		if (r < 10)
+		{
+			buffer[i] = r + '0';
+		}
+		else
+		{
+			buffer[i] = r - 10 + hex;
+		}
 		i++;
-	}
-	else
-	{
-		buffer[i] = r - 10 + hex;
-		i++;
-	}
 
-	n /= 2;
+		n /= 2;
 	}
 	buffer[i] = '\0';
 	reverse_str(buffer);
